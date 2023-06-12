af67feca406363204a44eb0a56cb18dfd733bd59
jinnit <jinnit810402@gmail.com>
2023-06-12 11:18:49 +0800

    加入專案檔案。

---
+++ Home_Work2/Home_Page.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home_Work2
+{
+    public partial class Home_Page : Form
+    {
+        public Home_Page()
+        {
+            InitializeComponent();
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            button16.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home_Work1 frm = new Home_Work1();
+            frm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Home_work2 frm2 = new Home_work2();
+            frm2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Home_Work3 frm3 = new Home_Work3();
+            frm3.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Home_Work4 frm4 = new Home_Work4();
+            frm4.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Home_Work8 frm8 = new Home_Work8();
+            frm8.Show();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Home_Work10 frm10 = new Home_Work10();
+            frm10.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Home_Work frm08 = new Home_Work();
+            frm08.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Home_Work5 frm05 = new Home_Work5();
+            frm05.Show();
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Home_Work2/Home_Work11.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home_Work2
+{
+    public partial class Home_Work11 : Form
+    {
+        private PictureBox pictureBox;
+        private Timer timer;
+        
+        private int xVelocity = 5 ;
+        private int yVelocity = 5 ;
+        public Home_Work11()
+        {
+            InitializeComponent();
+
+            pictureBox = new PictureBox();
+            
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            Controls.Add(pictureBox);
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+
+
+
+        }
+        
+       
+        
+
+        private void MoveImage()
+        {
+            pictureBox.Location = new Point(pictureBox.Left + xVelocity, pictureBox.Top + yVelocity);
+
+            if (pictureBox.Left < 0 || pictureBox.Right >= this.ClientSize.Width)
+            {
+                xVelocity = -xVelocity;
+            }
+
+            if (pictureBox.Top < 0 || pictureBox.Bottom >=this.ClientSize.Height)
+            {
+                yVelocity = -yVelocity;
+            }
+           
+        }
+        private void Home_Work11_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            MoveImage();
+        }
+
+        
+
+        private void Home_Work11_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+             this.Close();
+        }
+
+        private void Home_Work11_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit();//按下任意鍵結束程序
+        }
+    }
+    }
+    
+
