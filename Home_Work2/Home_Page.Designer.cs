15391efe46e37594370768a17f1653a6f5521995
jinnit810402 <jinnit810402@gmail.com>
2023-06-12 23:40:31 +0800

    120612night

--- Home_Work2/Home_Page.Designer.cs
+++ Home_Work2/Home_Page.Designer.cs
@@ -183,7 +183,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "ForDo While_9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
             // 
             // button8
             // 
--- Home_Work2/Home_Page.cs
+++ Home_Work2/Home_Page.cs
@@ -24,54 +24,73 @@ namespace Home_Work2
             button15.Enabled = false;
             button16.Enabled = false;
         }
+        public void frmCall(Form frm)
+        {
+            while (splitContainer2.Panel2.Controls.Count > 0)
+                splitContainer2.Panel2.Controls[0].Dispose();
 
+            //非最上層
+            frm.TopLevel = false;
+
+            //顯示Form，要加上去才會顯示Form
+            frm.Visible = true;
+
+            //設定Form位置
+            frm.Top = 0;
+            frm.Left = 0;
+
+            splitContainer2.Panel2.Controls.Add(frm);
+            
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Home_Work1 frm = new Home_Work1();
-            frm.Show();
+            frmCall(frm);
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Home_work2 frm2 = new Home_work2();
-            frm2.Show();
+            frmCall(frm2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Home_Work3 frm3 = new Home_Work3();
-            frm3.Show();
+            frmCall(frm3);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             Home_Work4 frm4 = new Home_Work4();
-            frm4.Show();
+            frmCall(frm4);
         }
-
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Home_Work5 frm05 = new Home_Work5();
+            frmCall(frm05);
+        }
         private void button8_Click(object sender, EventArgs e)
         {
             Home_Work8 frm8 = new Home_Work8();
-            frm8.Show();
+            frmCall(frm8);
+        }
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            Home_Work frm09 = new Home_Work();
+            frmCall(frm09);
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             Home_Work10 frm10 = new Home_Work10();
-            frm10.Show();
+            frmCall(frm10);
         }
 
-        private void button9_Click(object sender, EventArgs e)
-        {
-            Home_Work frm08 = new Home_Work();
-            frm08.Show();
-        }
+        
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Home_Work5 frm05 = new Home_Work5();
-            frm05.Show();
-        }
+       
 
         private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
         {
@@ -83,5 +102,7 @@ namespace Home_Work2
             Home_Work11 frm11 = new Home_Work11();
             frm11.Show();
         }
+
+       
     }
 }
